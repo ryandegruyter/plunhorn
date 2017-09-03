85bc8634f72d6ec8ab843de66b20e532e5cc34f8
Ryan de Gruyter <ryan.degruyter@ordina.be>
2017-09-03 17:58:23 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <app-title></app-title>
+  </div>
+</template>
+
+<script lang="ts">
+  import AppTitle from './components/title.vue';
+  import Vue from 'vue'
+  import Component from 'vue-class-component'
+
+  @Component({
+    components:{
+      appTitle: AppTitle
+    }
+  })
+  export default class App extends Vue {
+  }
+</script>
+
+<style>
+  html, body {
+    height: 100%;
+  }
+
+  .title {
+    text-transform: uppercase;
+  }
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    background-color: #000;
+    height: 100%;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+</style>
---
+++ src/components/title.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <h1>Plunging Hornet</h1>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+  import Component from 'vue-class-component'
+
+  @Component({})
+  export default class AppTitle extends Vue {
+  }
+</script>
+
+<style scoped>
+  .container {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+  }
+
+  h1 {
+    color: #fff;
+    margin: 0;
+    text-transform: uppercase;
+  }
+</style>
